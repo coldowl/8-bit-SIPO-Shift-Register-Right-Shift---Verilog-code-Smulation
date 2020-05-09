66b7c7d730d69ab2f9f7f573e4392b21c518c4ee
Souvick Saha <souvicksaha95@gmail.com>
2020-05-09 19:23:28 +0530

    Update d_flipflop.v

--- d_flipflop.v
+++ d_flipflop.v
@@ -5,6 +5,6 @@ module d_flipflop (clk, D, Q, reset);
 	always @(posedge reset) // to reset the signal if MR_bar goes LOW.
 		Q <= 1'b0;
 	
-	always @(posedge clk)
+	always @(posedge clk)   // Pushing the value of D to Q at every positive edge of clk
 		Q <= D;
 endmodule
