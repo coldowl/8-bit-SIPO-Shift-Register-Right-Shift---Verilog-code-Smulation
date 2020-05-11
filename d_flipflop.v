259291f6010814d416fab4636ace144e2feb2422
Souvick Saha <souvicksaha95@gmail.com>
2020-05-11 20:04:36 +0530

    Update d_flipflop.v

--- d_flipflop.v
+++ d_flipflop.v
@@ -2,9 +2,9 @@ module d_flipflop (clk, D, Q, reset);
 	input clk, D, reset;
 	output reg Q;
 	
-	always @(posedge reset) // to reset the signal if MR_bar goes LOW.
+	always @(posedge reset) // to reset the signal if MR_bar goes LOW. It will clear all the FF outputs to 0.
 		Q <= 1'b0;
 	
-	always @(posedge clk)   // Pushing the value of D to Q at every positive edge of clk
+	always @(posedge clk)   // Pushing the value of D to Q at every positive edge of clk.
 		Q <= D;
 endmodule
